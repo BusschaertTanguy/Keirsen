fe4757756ba6d7c5afe635c88ea65ec84de495f2
BusschaertTanguy <tanguy.busschaert.95@gmail.com>
2022-04-22 19:51:22 +0200

    Update authorization.guard.ts
    remove console log

--- frontend/src/core/guards/authorization.guard.ts
+++ frontend/src/core/guards/authorization.guard.ts
@@ -11,7 +11,6 @@ export class AuthorizationGuard implements CanActivate {
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        console.log("GUARD");
         return this.authorizationService.isAuthenticated().pipe(tap((isAuthenticated: boolean) => {
             if (!isAuthenticated) {
                 this.router.navigate(["authentication", "login"]);
