702e63c4eabb079b889f7b0935debcd5542078e9
Tanguy Busschaert <devlup.srl@gmail.com>
2022-04-28 13:55:33 +0200

    remove unused guard

--- frontend/src/core/guards/authorization.guard.ts
+++ frontend/src/core/guards/authorization.guard.ts
@@ -1,4 +1,4 @@
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from "@angular/router";
 import {Observable} from "rxjs";
 import {AuthorizationService} from "../services/authorization.service";
 import {Injectable} from "@angular/core";
@@ -7,7 +7,7 @@ import {Injectable} from "@angular/core";
     providedIn: "root"
 })
 export class AuthorizationGuard implements CanActivate {
-    public constructor(private readonly authorizationService: AuthorizationService, private router: Router) {
+    public constructor(private readonly authorizationService: AuthorizationService) {
     }
 
     public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
